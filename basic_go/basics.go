572a3d8e722929f0eb72e66c28dc59397e51786e
emirot <emirot.nolan@gmail.com>
2020-12-09 21:32:58 -0800

    feat: add example

--- basic_go/basics.go
+++ basic_go/basics.go
@@ -8,12 +8,18 @@ func firstFunc() {
 	fmt.Println("First print")
 }
 
-func firstRetFunc() (string) {
+func firstRetFunc() string {
 	return "a string returnned"
 }
 
+func returnWithNames() (first string, second string) {
+	first := "first"
+	second := "second"
+	return
+}
 
 func main() {
 	firstFunc()
 	fmt.Println(firstRetFunc())
+	fmt.Println(returnWithNames())
 }
