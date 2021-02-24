ebea0779b4bc2b0d5f5e2496f79699ee8253f194
nolan emirot <emirot.nolan@gmail.com>
2021-02-24 09:11:59 -0800

    test

--- basic_go/basics.go
+++ basic_go/basics.go
@@ -12,14 +12,20 @@ func firstRetFunc() string {
 	return "a string returnned"
 }
 
+func secondFunc() {
+	a := 11
+	fmt.Println(a)
+}
+
 func returnWithNames() (first string, second string) {
-	first := "first"
-	second := "second"
+	first = "first"
+	//second = "second2"
 	return
 }
 
 func main() {
 	firstFunc()
+	secondFunc()
 	fmt.Println(firstRetFunc())
 	fmt.Println(returnWithNames())
 }
